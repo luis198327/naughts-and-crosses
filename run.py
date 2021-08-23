28ffdc410def9e3bdd4b9d5f92aa7c6dcf941433
luis198327 <luis.brites@sky.com>
2021-08-23 18:14:40 +0000

    Add initial functions for game and process flow to assets directory

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Initial Code functions required:
+
+# def play_game():
+
+# def display_game_area():
+
+# def current_player_input():
+
+# def change_player():
+
+# def has_game_finished():
+
+# def check_if_winner():
+
+# def check_row():
+
+# def check_column():
+
+# def check_diagonal():
+
+# def check_if_draw():
+
+# def restart_game():
