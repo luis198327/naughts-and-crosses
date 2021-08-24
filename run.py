333d9dc5bf15b94fd5976da3ff00f78a3e4533b1
luis198327 <luis.brites@sky.com>
2021-08-24 18:10:21 +0000

    Add while loop to play_game with 3 placeholder functions

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ grid = [' - ', ' - ', ' - ',
         ' - ', ' - ', ' - ',
         ' - ', ' - ', ' - ']
 
+game_active = True
+
 
 def display_game_area():
     """
@@ -15,8 +17,8 @@ def display_game_area():
 def current_player_input():
     """
     Request current player to input their grid slot within the
-    grid area. Changes to integer and minus one to get correct
-    index from grid list
+    grid area. Change to integer and minus one to get correct
+    index from grid list. Places character in grid slot.
     """
     grid_slot = int(input("\nEnter grid number between 1-9:\n"))-1
 
@@ -25,28 +27,31 @@ def current_player_input():
     display_game_area()
 
 
+# def change_player():
+
+
+# def has_game_finished():
+
+
 def play_game():
     """
     Start game
     """
     display_game_area()
+    
+    while game_active:
+        
+        current_player_input()
 
-    current_player_input()
+        has_game_finished():
+
+        change_player():
 
 
 play_game()
 
 
-
-
-
-
-
 # Initial Code functions required:
-
-# def change_player():
-
-# def has_game_finished():
 
 # def check_if_winner():
 
