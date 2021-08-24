f081437710f53fd1bced9c450c86043f8a8507e7
luis198327 <luis.brites@sky.com>
2021-08-24 17:54:56 +0000

    Add player input function to write character to grid

--- run.py
+++ run.py
@@ -5,11 +5,24 @@ grid = [' - ', ' - ', ' - ',
 
 def display_game_area():
     """
-    Display and print the gaming area
+    Display and print the gaming area.
     """
     print('| ' + grid[0] + ' | ' + grid[1] + ' | ' + grid[2] + ' | ')
     print('| ' + grid[3] + ' | ' + grid[4] + ' | ' + grid[5] + ' | ')
     print('| ' + grid[6] + ' | ' + grid[7] + ' | ' + grid[8] + ' | ')
+
+
+def current_player_input():
+    """
+    Request current player to input their grid slot within the
+    grid area. Changes to integer and minus one to get correct
+    index from grid list
+    """
+    grid_slot = int(input("\nEnter grid number between 1-9:\n"))-1
+
+    grid[grid_slot] = ' X '
+
+    display_game_area()
 
 
 def play_game():
@@ -18,15 +31,18 @@ def play_game():
     """
     display_game_area()
 
+    current_player_input()
+
 
 play_game()
 
+
+
+
+
+
+
 # Initial Code functions required:
-
-
-
-
-# def current_player_input():
 
 # def change_player():
 
